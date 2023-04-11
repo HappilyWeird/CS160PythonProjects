1e809a7f5d4e5be1de9cd2d81d724703bdb3f665
HappilyWeird <130501896+HappilyWeird@users.noreply.github.com>
2023-04-11 12:23:09 -0700

    Add files via upload
    Drawing a house.

--- test.py
+++ test.py
@@ -1,1 +1,26 @@
-nothing
+from turtle import *
+from time import sleep
+
+for i in range(4):
+	forward(100)
+	right(90)
+left(45)
+forward(71)
+right(90)
+forward(71)
+right(45)
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(40)
+right(90)
+forward(50)
+right(90)
+forward(450)
+right(180)
+forward(900)
+
+sleep(10)
